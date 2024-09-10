03173abc7bc34897a52c0d1f4f97aaf1136de207
LoloBalth <168077267+LoloBalth@users.noreply.github.com>
2024-09-10 14:16:58 +0200

    Update nails_vid.py

--- sections/nailsdetection/nails_vid.py
+++ sections/nailsdetection/nails_vid.py
@@ -154,8 +154,8 @@ def handle_asyncio_exceptions(loop):
     loop = asyncio.get_event_loop()
     handle_asyncio_exceptions(loop)
 
-#     # Run the nails detection page
-#     nails_page()
+ # Run the nails detection page
+  nails_page()
 
 # # Run the app
 # if __name__ == "__main__":
